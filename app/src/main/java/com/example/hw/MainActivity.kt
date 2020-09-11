564f24b80b6c40ae22597d8406e73525f45aab3b
Dina3308 <dina.red09@gmail.com>
2020-09-12 00:47:13 +0300

    Add class, interface

--- app/src/main/java/com/example/hw/MainActivity.kt
+++ app/src/main/java/com/example/hw/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val phone: Phone = Phone("Samsung Exynos 9610 Octa",6,"Samsung",17250, "Vietnam")
+        phone.off()
+        phone.on()
+        phone.getInfo()
     }
 }
