9b8cf4f78867096cd171a8cc035ce4c1972e7ed0
Dina3308 <57904472+Dina3308@users.noreply.github.com>
2021-01-01 20:06:13 +0300

    Update MainActivity.kt
    bug fix

--- app/src/main/java/com/example/hw/MainActivity.kt
+++ app/src/main/java/com/example/hw/MainActivity.kt
@@ -38,8 +38,9 @@ class MainActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE) {
-            val imageUri = data?.data
-            imageView.setImageURI(imageUri)
+            data?.data?.also {
+                imageView.setImageURI(it)
+            }    
         }
         else{
             Toast.makeText(this, "Task Cancelled", Toast.LENGTH_SHORT).show()
