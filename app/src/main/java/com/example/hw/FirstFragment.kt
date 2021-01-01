4f782eb6173f2a8ad111c1acf879a55158da5344
Dina3308 <57904472+Dina3308@users.noreply.github.com>
2021-01-01 20:49:26 +0300

    Update FirstFragment.kt

--- app/src/main/java/com/example/hw/FirstFragment.kt
+++ app/src/main/java/com/example/hw/FirstFragment.kt
@@ -8,11 +8,7 @@ import android.view.ViewGroup
 
 
 class FirstFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
